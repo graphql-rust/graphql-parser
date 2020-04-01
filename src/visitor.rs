dea17494efdc1d95eb9ba9eeb9ae5dad7f8efa11
Paul Colomiets <paul@colomiets.name>
2020-04-01 17:18:19 +0300

    Add visitor over document

--- src/visitor.rs
+++ src/visitor.rs
@@ -2,6 +2,7 @@ use std::slice;
 
 use crate::common::Text;
 use crate::query::{SelectionSet, Directive, Selection, Field};
+use crate::query::{Document, Definition};
 
 
 pub trait Visit {
@@ -73,11 +74,62 @@ impl<'a, T: 'a> Iterator for FieldIter<'a, T>
     }
 }
 
+#[derive(Debug)]
+pub struct DocumentFieldIter<'a, T>
+    where T: Text<'a>
+{
+    doc_iter: slice::Iter<'a, Definition<'a, T>>,
+    field_iter: Option<FieldIter<'a, T>>,
+}
+
+impl<'a, T> VisitorData for (&'a Document<'a, T>, &'a Field<'a, T>)
+    where T: Text<'a>,
+{
+    type Data = DocumentFieldIter<'a, T>;
+}
+
+impl<'a, T> CreateData<'a, &'a Document<'a, T>, &'a Field<'a, T>>
+    for DocumentFieldIter<'a, T>
+    where T: Text<'a>,
+{
+    fn new(v: &'a Document<'a, T>) -> Self {
+        Self {
+            doc_iter: v.definitions.iter(),
+            field_iter: None,
+        }
+    }
+}
+
+impl<'a, T: 'a> Iterator for DocumentFieldIter<'a, T>
+    where T: Text<'a>,
+{
+    type Item = &'a Field<'a, T>;
+    fn next(&mut self) -> Option<&'a Field<'a, T>> {
+        use crate::query::Definition::*;
+        use crate::query::OperationDefinition::*;
+        loop {
+            if let Some(field_iter) = &mut self.field_iter {
+                if let Some(result) = field_iter.next() {
+                    return Some(result);
+                }
+            }
+            self.field_iter.take();
+            let ss = match self.doc_iter.next() {
+                Some(Operation(SelectionSet(def))) => &def,
+                Some(Operation(Query(def))) => &def.selection_set,
+                Some(Operation(Mutation(def))) => &def.selection_set,
+                Some(Operation(Subscription(def))) => &def.selection_set,
+                Some(Fragment(def)) => &def.selection_set,
+                None => return None,
+            };
+            self.field_iter = Some(ss.visit::<Field<'a, T>>());
+        }
+    }
+}
+
 #[test]
 fn test_field_iter() {
     use crate::parse_query;
-    use crate::query::Definition::Operation;
-    use crate::query::OperationDefinition::Query;
 
     let doc = parse_query::<&str>(r#"
         query TestQuery {
@@ -89,16 +141,14 @@ fn test_field_iter() {
             }
         }
     "#).expect("Failed to parse query");
-    let def = match doc.definitions.iter().next().unwrap() {
-        Operation(Query(q)) => &q.selection_set,
-        _ => unreachable!(),
-    };
     let mut fields = 0;
     let mut field_names = Vec::new();
-    for f in def.visit::<Field<_>>() {
+    for f in doc.visit::<Field<_>>() {
         fields += 1;
         field_names.push(f.name);
     }
     assert_eq!(fields, 4);
     assert_eq!(field_names, vec!["users", "id", "country", "id"]);
 }
+
+
