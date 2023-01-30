0d93ac9310c2894a029d0eb912c3463875a535f9
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-01-30 17:01:04 +0100

    Introduce minify_query (#67)
    Useful to reduce the size of payload

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -4,8 +4,9 @@ mod ast;
 mod error;
 mod format;
 mod grammar;
-
+mod minify;
 
 pub use self::grammar::{parse_query, consume_definition};
 pub use self::error::ParseError;
 pub use self::ast::*;
+pub use self::minify::minify_query;
